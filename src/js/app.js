9b70ec60d82618df15da856bf107036d25539caa
Suley <suleyliu@gmail.com>
2020-05-19 13:28:18 -0500

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+let localLng;
+let localLat;
+
+
+navigator.geolocation.getCurrentPosition(success);
+
+function success(pos) {
+  const crd = pos.coords;
+  localLng = crd.longitude;
+  localLat = crd.latitude;
+  console.log(crd.longitude, crd.latitude)
+}
