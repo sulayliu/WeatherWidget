c57a790ee2db7b984682b002a32bb196dfc95208
Suley <suleyliu@gmail.com>
2020-05-19 14:59:01 -0500

    Get the latitude and longitude and fetch json data from the weather api.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+const apikey = `eeebbc3ed4485cdb52855a486de349b2`;
 let localLng;
 let localLat;
 
@@ -6,7 +7,22 @@ navigator.geolocation.getCurrentPosition(success);
 
 function success(pos) {
   const crd = pos.coords;
-  localLng = crd.longitude;
   localLat = crd.latitude;
+  localLng = crd.longitude;
   console.log(crd.longitude, crd.latitude);
+  getWeather(crd.latitude, crd.longitude)
+}
+
+function getWeather(lat, lon) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`)
+  .then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error("There is an error on street name.");
+    }
+  })
+  .then((json) => {
+    console.log(json);
+  })
 }
