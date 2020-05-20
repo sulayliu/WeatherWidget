5e2c69f9c4986b2445763b0ca6b670b9da623214
Suley <suleyliu@gmail.com>
2020-05-20 13:36:33 -0500

    Change the weekday order to output the correct day

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 const apikey = `eeebbc3ed4485cdb52855a486de349b2`;
-const weekday = [`Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, `Sunday`];
+const weekday = [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`];
 
 
 navigator.geolocation.getCurrentPosition(success);
@@ -57,22 +57,20 @@ function forecast(array) {
   let html = ``;
   const forecastEle = document.querySelector(`.forecast`);
   forecastEle.textContent = ``;
-
   for(let i = 0; i < 5; i++) {
     let newArray = [];
     date.setDate(date.getDate() + 1);
-    console.log(date.getDate());
-    console.log(weekday[date.getDay()]);
     for(let ele of array) {
       if (new Date(ele.dt_txt).getDate() === date.getDate()) {
         newArray.push(ele);
       }
     }
-    let maxTemp = [...newArray]
-    maxTemp.sort((a, b) => b.main.temp_max - a.main.temp_max);
-    console.log(maxTemp[0].main.temp_max);
-    let minTemp = newArray.sort((a, b) => a.main.temp_min - b.main.temp_min);
-    console.log(minTemp[0].main.temp_min);
+    newArray.sort((a, b) => b.main.temp_max - a.main.temp_max);
+    let maxTemp = newArray[0].main.temp_max;
+    console.log(maxTemp);
+    newArray.sort((a, b) => a.main.temp_min - b.main.temp_min);
+    let minTemp = newArray[0].main.temp_min
+    console.log(minTemp);
     let noonWeater = newArray.find(ele => new Date(ele.dt_txt).getHours() == 12);
     console.log(noonWeater);
     html += `<div class="day">
@@ -80,7 +78,7 @@ function forecast(array) {
         <img src="http://openweathermap.org/img/wn/${noonWeater.weather[0].icon}@2x.png">
         <div class="description">${noonWeater.weather[0].description}</div>
         <div class="temp">
-          <span class="high">${maxTemp[0].main.temp_max.toFixed(0)}℃</span>/<span class="low">${minTemp[0].main.temp_min.toFixed(0)}℃</span>
+          <span class="high">${maxTemp.toFixed(0)}℃</span>/<span class="low">${minTemp.toFixed(0)}℃</span>
         </div>
       </div>`
   }
